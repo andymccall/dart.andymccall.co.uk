ecad56fde81d5b109b7d3c55174ff565e34f8ba8
Andy McCall <mailme@andymccall.co.uk>
2018-03-08 12:58:35 +0000

    Added cars class

---
+++ bin/classes/carplay.dart
@@ -0,0 +1,20 @@
+import 'car.dart';
+
+main(List<String> arguments) {
+
+  var newCar = new Car();
+
+  newCar.paintColor = "silver";
+  newCar.addFuel(60);
+  newCar.turnOn();
+
+  print("Car is ${newCar.paintColor}");
+  print("Car has ${newCar.fuel} litres of fuel");
+
+  if (newCar.isRunning) {
+    print("Car is running");
+  } else {
+    print("Car is not running");
+  }
+
+}
